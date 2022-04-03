6c4b5ca2f3f2e86fb89e2a5f0f44fb7454aeff9e
tamaka <mob@tamaka.com>
2022-04-03 08:40:54 +0000

    fix:scssフォルダ内をlayoutsフォルダに移行

---
+++ layouts/base.php
@@ -0,0 +1,24 @@
+<?php
+ echo 'Hello World!';
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="base.css">
+    <script src="js/base.js"></script>
+</head>
+<body>
+<!-- header -->
+<?php 
+require_once("header.php")
+?>
+
+<!-- sidemenu -->
+<?php 
+require_once("sidemenu.php")
+?>
+</body>
+</html>
